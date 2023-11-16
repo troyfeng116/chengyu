2b78faa680557efea7b23b949fa4ba215980ecb1
Troy Feng <troyfeng1@gmail.com>
2023-11-15 22:36:18 -0500

    Add pinyin + fanyi to basic scraping

--- scraping/main.py
+++ scraping/main.py
@@ -1,15 +1,15 @@
 import requests
 import bs4
 
-from bs4 import BeautifulSoup
-from typing import Union
+from bs4 import BeautifulSoup, element
+from typing import List, Union
 
 
 # URL = "http://www.zd9999.com/cy/index.htm"
-URL = "http://www.zd9999.com/cy/htm0/1.htm"
+# URL = "http://www.zd9999.com/cy/htm0/1.htm"
 # URL = "http://www.zd9999.com/cy/htm0/4.htm"
 
-START_PATH = "/cy/htm0/1.htm"
+START_PATH = "/cy/htm0/1000.htm"
 BASE_URL = "http://www.zd9999.com"
 
 
@@ -21,15 +21,24 @@ def scrape(path: str) -> Union[str, None]:
 
     html = res.content.decode("gb2312", "ignore")
     soup = BeautifulSoup(html, "html.parser")
-    table: bs4.element.Tag = soup.find_all("table")[-3]
-    first_row: bs4.element.Tag = table.findChildren("tr")[0]
+
+    table: element.Tag = soup.find_all("table")[-3]
+    rows: List[element.Tag] = table.findChildren("tr")
+
+    first_row: element.Tag = rows[0]
     cheng_yu = first_row.findChild("td").findChild("font").findChild("b").getText()
-    print(f"{path} -> {cheng_yu}")
+
+    second_row: element.Tag = rows[1]
+    nested_rows: List[element.Tag] = second_row.findChild("td").findChildren("tr")
+    pinyin = nested_rows[0].findChildren("td")[1].getText()
+    fanyi = nested_rows[1].findChildren("td")[1].getText()
+
+    print(f"{path} -> {cheng_yu}: {pinyin} -> {fanyi}")
 
     try:
-        last_row: bs4.element.Tag = table.findChildren("tr")[-1]
-        last_cell: bs4.element.Tag = last_row.findChildren("td")[-1]
-        next_link_el: bs4.element.Tag = last_cell.findChild("a")
+        last_row: element.Tag = rows[-1]
+        last_cell: element.Tag = last_row.findChildren("td")[-1]
+        next_link_el: element.Tag = last_cell.findChild("a")
         next_link_href = next_link_el.get("href")
         return next_link_href
     except:
